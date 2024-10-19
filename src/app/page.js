58bd2e1df92f3d8583a64552021789c738797d95
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-10-20 00:31:48 +0200

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -11,16 +11,16 @@ export default async function Home({ searchParams }) {
       <h1 className='py-10 text-3xl text-blue-500 text-center border-b-4 border-b-blue-500'>
         NextJS: contenido dinámico
       </h1>
-      <form className='my-10 grid grid-cols-[100px_auto] gap-4'>
+      <form className='my-10 grid grid-cols-[150px_auto] gap-4'>
 
         <label htmlFor='nombre'>Nombre</label>
-        <input id='nombre' name='nombre' className='p-1 border border-slate-200' />
+        <input id='nombre' name='nombre' className='p-1 border border-slate-200 focus:outline-blue-300 text-lg' />
 
         <label htmlFor='descripcion'>Descripcion:</label>
-        <input id='descripcion' name='descripcion' className='p-1 border border-slate-200' />
+        <input id='descripcion' name='descripcion' className='p-1 border border-slate-200 focus:outline-blue-300 text-lg' />
 
         <label htmlFor='precio'>Precio</label>
-        <input id='precio' name='precio' type='number' step='0.01' className='p-1 border border-slate-200' />
+        <input id='precio' name='precio' type='number' step='0.01' className='p-1 border border-slate-200 focus:outline-blue-300 text-lg' />
 
         <div className='col-span-2 grid gap-2'>
           <button formAction={createProducto} className='bg-green-600 text-white px-4 py-2 rounded-xl'>
--- src/components/buscar.jsx
+++ src/components/buscar.jsx
@@ -2,11 +2,10 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { useDebouncedCallback } from 'use-debounce';
 
-function Buscar () {
+function Buscar() {
     const searchParams = useSearchParams()
     const pathname = usePathname();
     const { replace } = useRouter();
-
 
     const handleSearch = useDebouncedCallback((text) => {
         const params = new URLSearchParams(searchParams);
@@ -21,12 +20,12 @@ function Buscar () {
 
     return (
         <div className="text-right">
-            🔍 <input
+            <input
                 type='search'
-                className={`text-black p-2 pl-10 rounded-full my-5 bg-[length:16px_16px] bg-[center_left_10px] bg-no-repeat focus:outline-blue-300`}
-                placeholder={'Buscar por nombre...'}
+                className={`text-black p-2 pl-10 rounded-full my-5 bg-[url('/search.svg')] bg-[length:16px_16px] bg-[center_left_10px] bg-no-repeat border border-slate-200 focus:outline-blue-300`}
+                placeholder={'Buscar ...'}
                 onChange={(e) => handleSearch(e.target.value)}
-                defaultValue={ searchParams.get('query') }
+                defaultValue={searchParams.get('query')}
             />
         </div>
     )
